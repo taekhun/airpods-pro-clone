9d6e2edbbe523a21e36cb6eed953abce47366b34
taekhun <nth9708@naver.com>
2021-02-16 21:11:30 +0900

    Init

---
+++ app.js
@@ -0,0 +1,42 @@
+const intro = document.querySelector(".intro");
+const video = intro.querySelector("video");
+const text = intro.querySelector("h1");
+//END SECTION
+const section = document.querySelector("section");
+const end = section.querySelector("h1");
+
+//SCOROLL MAGIC
+const controller = new ScrollMagic.Controller();
+
+let scene = new ScrollMagic.Scene({
+  duration: 5700,
+  triggerElement: intro,
+  triggerHook: 0,
+})
+  .setPin(intro)
+  .addTo(controller);
+
+//Text Animation
+const textAnim = TweenMax.fromTo(text, 3, { opacity: 1 }, { opacity: 0 });
+
+let scene2 = new ScrollMagic.Scene({
+  duration: 3000,
+  triggerElement: intro,
+  triggerHook: 0,
+})
+  .setTween(textAnim)
+  .addTo(controller);
+
+//Video Animation
+let accelAmount = 0.9;
+let scrollPos = 0;
+let delay = 0;
+
+scene.on("update", (e) => {
+  scrollpos = e.scrollPos / 1000;
+});
+
+setInterval(() => {
+  delay += (scrollpos - delay) * accelAmount;
+  video.currentTime = delay;
+}, 60);
